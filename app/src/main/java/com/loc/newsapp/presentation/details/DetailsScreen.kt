51792b3f453ccabc120737b85679317fd9614da7
asadbek <aaminboyev2@gmail.com>
2025-11-19 15:38:02 +0500

    add details screen

---
+++ app/src/main/java/com/loc/newsapp/presentation/details/DetailsScreen.kt
@@ -0,0 +1,129 @@
+package com.loc.newsapp.presentation.details
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.loc.newsapp.R
+import com.loc.newsapp.domain.model.Article
+import com.loc.newsapp.domain.model.Source
+import com.loc.newsapp.presentation.Dimens.ArticleImageHeight
+import com.loc.newsapp.presentation.Dimens.MediumPadding1
+import com.loc.newsapp.presentation.details.components.DetailsTopBar
+import com.loc.newsapp.ui.theme.NewsAppTheme
+
+@Composable
+fun DetailsScreen(
+    article: Article,
+    event: (DetailsEvent) -> Unit,
+    navigateUp: () -> Unit
+) {
+
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+        DetailsTopBar(
+            onBrowsingClick = {
+                Intent(Intent.ACTION_VIEW).also {
+                    it.data = Uri.parse(article.url)
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onBookmarkClick = {
+                event(DetailsEvent.SaveArticle)
+            },
+            onShareClick = {
+                Intent(Intent.ACTION_SEND).also {
+                    it.data = Uri.parse(article.url)
+                    it.type = "text/plain"
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onBackClick = navigateUp
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(
+                start = MediumPadding1,
+                end = MediumPadding1,
+                top = MediumPadding1,
+            )
+        ) {
+            item {
+                AsyncImage(
+                    model = ImageRequest.Builder(context = context).data(article.urlToImage).build(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(ArticleImageHeight)
+                        .clip(MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(Modifier.height(MediumPadding1))
+
+                Text(
+                    text = article.title,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = colorResource(R.color.text_title)
+                )
+
+                Text(
+                    text = article.content,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = colorResource(R.color.body)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DetailsScreenPrev() {
+    NewsAppTheme(dynamicColor = false) {
+        DetailsScreen(
+            article = Article(
+                author = "",
+                content = "",
+                description = "",
+                publishedAt = "",
+                source = Source(
+                    id = "",
+                    name = "bbc"
+                ),
+                title = "",
+                url = "",
+                urlToImage = ""
+            ),
+            event = {},
+            navigateUp = {}
+        )
+    }
+}
